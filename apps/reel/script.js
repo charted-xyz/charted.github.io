8c0971140f45962b69d53221098d49b1c2c8ed5b
Andriy Gazin <andriy-gazin@users.noreply.github.com>
2021-02-18 01:42:27 +0200

    update reel

--- apps/reel/script.js
+++ apps/reel/script.js
@@ -1,104 +1,86 @@
 async function showMovies() {
+  // read the data
 
-    // read the data
+  let data = await d3.csv("data/06_data_for_mosaic.csv", d3.autoType);
 
-    let data = await d3.csv('data/06_data_for_mosaic.csv', d3.autoType)
+  // append images to the movies, show additional information on hover
 
-    // append images to the movies, show additional information on hover
+  const movieCard = d3.select(".movie");
 
-    const movieCard = d3.select('#movieCard')
+  d3.select("#movies")
+    .selectAll("div")
+    .data(data)
+    .enter()
+    .append("div")
+    .attr("class", "movie-container")
+    .style("background-image", (d) => `url(img/${d.img_title})`)
+    .append("div")
+    .attr("class", "movie-card")
+    .html(
+      (d) =>
+        `<div class='movie'>
+            <span class='movie-title'>${d.title}</span> 
+            <br> by ${d.director} 
+            <br> 
+            Released in ${d.release_year}
+            <br>
+            History grade: ${d.history}
+            <br>
+            Entertainment grade: ${d.entertainment}
+            </div>
+            <div class='movie-url'>
+            <br>
+            <p><a href=${d.imdb_url} target="_blank">Read more on IMDB</a></p>
+            </div>`
+    );
 
-    d3.select('#movies')
-        .selectAll('div')
-        .data(data)
-        .enter()
-        .append('div')
-        .attr('class', 'movie')
-        .append('a')
-        .attr('href', d => d.imdb_url)
-        .attr('target', '_blank')
-        .append('img')
-        .attr('src', d => `img/${d.img_title}`)
-        .attr('alt', d => `${d.title} by ${d.director}`)
-        .on('mouseenter', function (event, d) {
+  const buttons = d3.selectAll("#sidebar button");
+  let order = "ascending";
 
-            movieCard
-                .style('opacity', 1);
+  buttons.on("click", function (event) {
+    d3.selectAll("#sidebar button")
+      .classed("selected", false)
+      .select("span")
+      .text("");
+    d3.select(event.currentTarget).classed("selected", true);
 
-            movieCard
-                .select('#movieTitle')
-                .text(d.title);
+    document.getElementById("movies").scrollTop = 0;
 
-            movieCard
-                .select('#movieDirector')
-                .text(` by ${d.director}`);
+    if (order === "descending") {
+      order = "ascending";
+      d3.select(event.currentTarget).select("span").text(" 🠕"); // &#129045
+    } else {
+      order = "descending";
+      d3.select(event.currentTarget).select("span").text(" 🠗"); // &#129047
+    }
 
-            movieCard
-                .select('#releaseYear')
-                .text(`Released in ${d.release_year}`);
-
-            movieCard
-                .select('#historyGrade')
-                .text(`History grade: ${d.history}`);
-
-            movieCard
-                .select('#entertainmentGrade')
-                .text(`Entertainment grade: ${d.entertainment}`);
-
-        }).on('mouseleave', function (event) {
-
-            movieCard
-                .style('opacity', 0);
-
-        });
-
-    const buttons = d3.selectAll('#sidebar button');
-    let order = 'ascending';
-
-    buttons.on('click', function (event) {
-
-        d3.selectAll('#sidebar button').classed('selected', false).select('span').text('');
-        d3.select(event.currentTarget).classed('selected', true);
-
-        document.getElementById('movies').scrollTop = 0;
-
-        if (order === 'descending') {
-            order = 'ascending';
-            d3.select(event.currentTarget).select('span').text(' 🠕'); // &#129045
-        } else {
-            order = 'descending';
-            d3.select(event.currentTarget).select('span').text(' 🠗'); // &#129047
-        };
-
-        d3.selectAll('.movie')
-            .sort((a, b) => d3[order](a[d3.select(event.currentTarget).attr('id')], b[d3.select(event.currentTarget).attr('id')]));
-
-    });
+    d3.selectAll(".movie-container").sort((a, b) =>
+      d3[order](
+        a[d3.select(event.currentTarget).attr("id")],
+        b[d3.select(event.currentTarget).attr("id")]
+      )
+    );
+  });
 }
 
 // check screen size on load, say sorry if screen size is too small for us to proceed
 
 function sorryNotSorry() {
+  if (d3.min([window.innerWidth, window.innerHeight]) < 760) {
+    d3.select("main").remove();
 
-    if (d3.min([window.innerWidth, window.innerHeight]) < 760) {
+    d3.select("article")
+      .append("main")
+      .append("div")
+      .attr("class", "sorrynotsorry")
+      .html(
+        "<p>I haven't figured out how this thing should work on small screens yet. While I'm working on that, you can check this sketch on your computer or take a look at <a href='https://charted.xyz'>my other stuff</a>.<br><br>Anyway, have a nice day!</p>"
+      );
 
-        d3.select('main')
-            .remove();
-
-        d3.select('article')
-            .append('main')
-            .append('div')
-            .attr('class', 'sorrynotsorry')
-            .html("<p>I haven't figured out how this thing should work on small screens yet. While I'm working on that, you can check this sketch on your computer or take a look at <a href='https://charted.xyz'>my other stuff</a>.<br><br>Anyway, have a nice day!</p>");
-
-        d3.select('body')
-            .style('overflow', 'hidden');
-
-    } else {
-
-        showMovies();
-
-    }
+    d3.select("body").style("overflow", "hidden");
+  } else {
+    showMovies();
+  }
 }
 
 window.onload = sorryNotSorry();
