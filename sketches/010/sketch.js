9b1256b90d575d96bd15d3857d969d2afc2f2680
Andriy Gazin <andriy-gazin@users.noreply.github.com>
2022-02-01 00:14:44 +0200

    add sketch 010

---
+++ sketches/010/sketch.js
@@ -0,0 +1,89 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(3);
+  background('#0D1F30');
+  stroke('#DEBA64');
+  fill('#DEBA64');
+  noLoop();
+  
+  drawingContext.shadowOffsetX = 1;
+  drawingContext.shadowOffsetY = -1;
+  drawingContext.shadowBlur = 3;
+  drawingContext.shadowColor = '#08121Bk';
+}
+
+function draw() {
+
+  // define graphic parameters
+  const margin = 100;
+  const size = 8;
+  const radius = Math.min((width - margin * 2) / 2, (height - margin * 2) / 2);
+  const minDist = 10;
+
+  // generate array of random numbers
+
+  let data = makeData(1000, margin, width - margin, margin, height - margin);
+
+  // filter array so it only contains points that fit in the circle
+
+  let dataShaped = data.filter(point => abs(dist(width / 2, height / 2,
+    point.x, point.y)) < radius);
+
+  // connect dots
+  
+  for (let i = 0; i < dataShaped.length; i++) {
+
+    let firstPoint = dataShaped[i];
+    
+    let restOfTheData = dataShaped.slice();
+    
+    // find closes point to the previously picked random point
+        
+    restOfTheData.sort(function(a, b) {
+      return abs(dist(a.x, a.y, firstPoint.x, firstPoint.y)) - 
+        abs(dist(b.x, b.y, firstPoint.x, firstPoint.y));
+    });
+    
+    let secondPoint = restOfTheData[1];
+    
+    // connect two points with a line
+    
+    if(abs(dist(firstPoint.x, firstPoint.y, secondPoint.x, secondPoint.y)) > minDist) {
+      
+       strokeWeight(map(dist(secondPoint.x, secondPoint.y,
+                  width/2, height/2), 0, radius, size/2, size/8));
+       
+       line(firstPoint.x, firstPoint.y, secondPoint.x, secondPoint.y);
+       
+       }
+
+  }
+  
+  // draw points
+
+  for (let i = 0; i < dataShaped.length; i++) {
+
+    circle(dataShaped[i].x, dataShaped[i].y, 
+          map(dist(dataShaped[i].x, dataShaped[i].y,
+                  width/2, height/2), 0, radius, size, size/4));
+
+  }
+
+}
+
+
+// generate array of random numbers
+
+function makeData(length, startWidth, endWidth, startHeight, endHeight) {
+
+  let array = [];
+
+  for (i = 0; i < length; i++) {
+
+    array.push(createVector(random(startWidth, endWidth),
+      random(startHeight, endHeight)));
+
+  }
+
+  return (array)
+}
